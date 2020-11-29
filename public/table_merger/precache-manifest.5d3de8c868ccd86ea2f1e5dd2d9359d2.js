9b55f6ea7e8d05ba92990878b6d15e74d963102a
Arsen Shamkhalov <arsen.shamhalov@live.com>
2020-11-29 11:25:58 +0300

    Update table merger

---
+++ public/table_merger/precache-manifest.5d3de8c868ccd86ea2f1e5dd2d9359d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c88790813eddac869b512570ccf2cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7bb50fc4503a50e10d6",
+    "url": "/static/css/main.73a2b228.chunk.css"
+  },
+  {
+    "revision": "209435d5232a1b059471",
+    "url": "/static/js/2.41561dcd.chunk.js"
+  },
+  {
+    "revision": "a48b9dec06f0593901750484554745cd",
+    "url": "/static/js/2.41561dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7bb50fc4503a50e10d6",
+    "url": "/static/js/main.b4a92cc9.chunk.js"
+  },
+  {
+    "revision": "9652698a4f6c5c622052",
+    "url": "/static/js/runtime-main.525ece66.js"
+  }
+]);
--- public/table_merger/service-worker.js
+++ public/table_merger/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.050e31628e7efa66327a34cd60db29c6.js"
+  "/precache-manifest.5d3de8c868ccd86ea2f1e5dd2d9359d2.js"
 );
 
 self.addEventListener('message', (event) => {
